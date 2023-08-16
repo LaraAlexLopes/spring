81ee44f20f412a0b1e0ac13594d3dca68e54b218
Lara Lopes <laraalexandralopes919@gmail.com>
2023-08-16 17:39:41 -0300

    Deletar e Inserir

--- src/main/java/com/educandoweb/course/servicies/UserService.java
+++ src/main/java/com/educandoweb/course/servicies/UserService.java
@@ -1,5 +1,4 @@
 package com.educandoweb.course.servicies;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -14,12 +13,20 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
 	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 }
